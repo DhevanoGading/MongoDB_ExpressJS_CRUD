e31a1f59c58385c19e1c372ecfcb7c14f17a2246
Dhevano Gading <dhevanogading@gmail.com>
2023-07-09 23:25:00 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,190 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const { body, validationResult, check, Result } = require("express-validator");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+const methodOverride = require("method-override");
+
+require("./utils/db");
+const Contact = require("./model/contact");
+
+const app = express();
+const port = 3000;
+
+app.use(methodOverride("_method"));
+
+//setup ejs
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+//konfigurasi flash
+app.use(cookieParser("secret"));
+app.use(
+  session({
+    cookie: { maxAge: 6000 },
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    layout: "layouts/main-layout",
+    title: "Halaman Home",
+  });
+});
+
+//Halaman About
+app.get("/about", (req, res) => {
+  res.render("about", {
+    layout: "layouts/main-layout",
+    title: "Halaman About",
+  });
+});
+
+//Halaman Contact
+app.get("/contact", async (req, res) => {
+  const contacts = await Contact.find();
+
+  res.render("contact", {
+    layout: "layouts/main-layout",
+    title: "Halaman Contact",
+    contacts,
+    msg: req.flash("msg"),
+  });
+});
+
+//Tambah Contact
+app.get("/contact/add", (req, res) => {
+  res.render("add-contact", {
+    layout: "layouts/main-layout",
+    title: "Form Tambah Contact",
+  });
+});
+
+//Save contact
+app.post(
+  "/contact",
+  [
+    body("nama").custom(async (nama) => {
+      const duplikat = await Contact.findOne({ nama: nama });
+      if (duplikat) {
+        throw new Error("Nama contact sudah digunakan!");
+      }
+      return true;
+    }),
+    check("email", "Email tidak valid!").isEmail(),
+    check("nohp", "Nomon HP tidak valid!").isMobilePhone("id-ID"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // return res.status(400).json({ errors: errors.array() });
+      res.render("add-contact", {
+        layout: "layouts/main-layout",
+        title: "Form Tambah Contact",
+        errors: errors.array(),
+      });
+    } else {
+      // Contact.insertMany(req.body, (error, result) => {
+      //   if (error) {
+      //     console.log(error);
+      //     return res
+      //       .status(500)
+      //       .send("Terjadi kesalahan dalam menyimpan data.");
+      //   }
+      //   req.flash("msg", "Data Contact berhasil ditambahkan!");
+      //   res.redirect("/contact");
+      // });
+      try {
+        const newContact = new Contact(req.body);
+        await newContact.save();
+        req.flash("msg", "Data Contact berhasil ditambahkan!");
+        res.redirect("/contact");
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send("Terjadi kesalahan dalam menyimpan data.");
+      }
+    }
+  }
+);
+
+//Delete Contact
+app.delete("/contact", async (req, res) => {
+  Contact.deleteOne({ nama: req.body.nama }).then((result) => {
+    req.flash("msg", "Data Contact berhasil dihapus!");
+    res.redirect("/contact");
+  });
+});
+
+//Edit Contact
+app.get("/contact/edit/:nama", async (req, res) => {
+  const contact = await Contact.findOne({ nama: req.params.nama });
+
+  res.render("edit-contact", {
+    layout: "layouts/main-layout",
+    title: "Form Edit Contact",
+    contact,
+  });
+});
+
+//Save Edit Contact
+app.put(
+  "/contact",
+  [
+    body("nama").custom(async (value, { req }) => {
+      const duplikat = await Contact.findOne({ nama: value });
+      if (value !== req.body.oldNama && duplikat) {
+        throw new Error("Nama contact sudah digunakan!");
+      }
+      return true;
+    }),
+    check("email", "Email tidak valid!").isEmail(),
+    check("nohp", "Nomon HP tidak valid!").isMobilePhone("id-ID"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("edit-contact", {
+        layout: "layouts/main-layout",
+        title: "Form Edit Contact",
+        errors: errors.array(),
+        contact: req.body,
+      });
+    } else {
+      Contact.updateOne(
+        { _id: req.body._id },
+        {
+          $set: {
+            nama: req.body.nama,
+            email: req.body.email,
+            nohp: req.body.nohp,
+          },
+        }
+      ).then((result) => {
+        req.flash("msg", "Data Contact berhasil diubah!");
+        res.redirect("/contact");
+      });
+    }
+  }
+);
+
+//Detail Contact
+app.get("/contact/:nama", async (req, res) => {
+  const contact = await Contact.findOne({ nama: req.params.nama });
+
+  res.render("detail", {
+    layout: "layouts/main-layout",
+    title: "Halaman Detail Contact",
+    contact,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Mongo Contact App | listening at http://localhost:${port}`);
+});
